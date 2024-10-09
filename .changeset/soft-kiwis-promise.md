a1c0424910cb56f4033b5e2a6ffb534bef5e68c3
Simon <wsj_simon@163.com>
2024-10-09 21:23:58 +0800

    feat: eslint add react react-hook import plugins

---
+++ .changeset/soft-kiwis-promise.md
@@ -0,0 +1,5 @@
+---
+'@yueqing/eslint-config-ts': patch
+---
+
+add react react-hook import plugins
--- packages/eslint-config-ts/index.d.ts
+++ packages/eslint-config-ts/index.d.ts
@@ -1,2 +1,5 @@
-declare const _default: import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray;
+declare const _default: (params: {
+  project: string[] | boolean | string | null;
+  rootDir: string;
+}) => import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray;
 export default _default;
