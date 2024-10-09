315780ea4e91184cd14b731494244f9beb5d6523
Simon <wsj_simon@163.com>
2024-10-09 21:23:58 +0800

    feat: eslint add react react-hook import plugins

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,11 +1,92 @@
 // @ts-check
 
 import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  prettierConfig
-);
+const tsAndTsxFile = '**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}';
+
+/**
+ * @param {{ project: string[] | boolean | string | null, rootDir: string }} params - 函数参数
+ */
+export default ({ project, rootDir }) => {
+  const reactConfig = [
+    /** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.Config} */ ({
+      files: [tsAndTsxFile],
+      ...reactPlugin.configs.flat.recommended,
+      languageOptions: {
+        ...reactPlugin.configs.flat.recommended.languageOptions,
+        globals: {
+          ...globals.serviceworker,
+          ...globals.browser,
+        },
+      },
+    }),
+    {
+      files: [tsAndTsxFile],
+      ...jsxA11y.flatConfigs.recommended,
+      languageOptions: {
+        ...jsxA11y.flatConfigs.recommended.languageOptions,
+        globals: {
+          ...globals.serviceworker,
+          ...globals.browser,
+        },
+      },
+    },
+    /** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.Config} */ (
+      reactPlugin.configs.flat['jsx-runtime']
+    ),
+    {
+      files: [tsAndTsxFile],
+      plugins: {
+        'react-hooks': reactHooksPlugin,
+      },
+      rules: {
+        ...reactHooksPlugin.configs.recommended.rules,
+      },
+    },
+  ];
+
+  return tseslint.config(
+    js.configs.recommended,
+    importPlugin.flatConfigs.recommended,
+    importPlugin.flatConfigs.typescript,
+    ...reactConfig,
+    ...tseslint.configs.recommendedTypeChecked,
+    prettierConfig,
+    {
+      languageOptions: {
+        parserOptions: {
+          // projectService: true,
+          project: project,
+          tsconfigRootDir: rootDir,
+        },
+      },
+    },
+    {
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          typescript: {
+            project: project,
+          },
+          node: true,
+        },
+      },
+    },
+    {
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    }
+  );
+};
---
+++ packages/eslint-config-ts/types/eslint-plugin-react-hooks.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @see https://github.com/facebook/react/issues/30119
+ */
+declare module 'eslint-plugin-react-hooks' {
+  import type { ESLint } from 'eslint';
+  const plugin: Omit<ESLint.Plugin, 'configs'> & {
+    // eslint-plugin-react-hooks does not use FlatConfig yet
+    configs: Record<string, ESLint.ConfigData>;
+  };
+  export default plugin;
+}
