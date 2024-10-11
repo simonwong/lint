46af5ebe4c929863a379143cfd2b9a37cfa7749e
Simon <wsj_simon@163.com>
2024-10-11 11:23:23 +0800

    feat: use TSX build file

--- packages/eslint-config-ts/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: (params: {
-  project: string[] | boolean | string | null;
-  rootDir: string;
-}) => import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray;
-export default _default;
